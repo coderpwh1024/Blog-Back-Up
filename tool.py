248030773c4d671d5341f5f8a1ebf9089b0118c6
coder-PengWenHao <pengwenhao@aliyun.com>
2018-03-24 23:31:36 +0800

    update python

--- tool.py
+++ tool.py
@@ -192,7 +192,7 @@ def git_operation():
 if __name__ == "__main__":
     cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
     compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
-    # git_operation()    # 提交到github仓库
+    git_operation()    # 提交到github仓库
     handle_photo()     # 将文件处理成json格式，存到博客仓库中
     
     
