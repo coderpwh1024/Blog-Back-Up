a95908fc1a037fd0194aafc797d534ea41e0ec5c
coder-PengWenHao <pengwenhao@aliyun.com>
2018-03-25 09:58:15 +0800

    update hexo

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/coder-PengWenHao/Blog-Back-Up/blob/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/coder-PengWenHao/Blog-Back-Up/blob/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/coder-PengWenHao/Blog-Back-Up/master/photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/coder-PengWenHao/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
