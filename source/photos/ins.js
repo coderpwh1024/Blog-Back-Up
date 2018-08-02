47cf675551e52c7e98ba37f240a5b63fe1c12a21
coder-PengWenHao <pengwenhao@aliyun.com>
2018-08-02 14:51:01 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/coder-PengWenHao/Blog-Back-Up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/coder-PengWenHao/Blog-Back-Up/master/min_photos/' + data.link[i];
           var src = 'https://raw.githubusercontent.com/coder-PengWenHao/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
